3a5b85ad6bc5c3de923dc2d84b9a0232b9dc9f43
inn-u <inna.yu10@gmail.com>
2025-06-02 15:05:42 +0300

    improvements for user models, install bandit

--- users/models.py
+++ users/models.py
@@ -1,50 +1,36 @@
-# from django.contrib.auth.base_user import AbstractBaseUser
-# from django.contrib.auth.models import PermissionsMixin
-# from django.db import models
-#
-# from utils.model_behaviour import Timestampable
-#
-# from .manager import UserManager
-#
-#
-# class User(AbstractBaseUser, PermissionsMixin):
-#     email = models.EmailField(unique=True)
-#     is_active = models.BooleanField(
-#         default=True, verbose_name='Active',
-#         help_text='Is user active or not'
-#     )
-#     is_staff = models.BooleanField(
-#         default=False, verbose_name='Staff status',
-#         help_text='Is user staff of client'
-#     )
-#     is_blocked = models.BooleanField(
-#         default=False, verbose_name='Blocked',
-#         help_text='Is user blocked'
-#     )
-#
-#     objects = UserManager()
-#
-#     USERNAME_FIELD = 'email'
-#     REQUIRED_FIELDS = []
-#
-#     class Meta:
-#         verbose_name = 'user'
-#         verbose_name_plural = 'users'
-#
-#
-# class Client(Timestampable):
-#     user = models.OneToOneField(
-#         User, related_name='client', on_delete=models.CASCADE
-#     )
-#     first_name = models.CharField(max_length=50)
-#     last_name = models.CharField(max_length=100)
-#     date_of_birth = models.DateField(
-#         verbose_name='Date of Birth', null=True, blank=True
-#     )
-#
-#     def __str__(self):
-#         return f'user_id={self.user.id} email={self.user.email}'
-#
-#     @property
-#     def email(self) -> str:
-#         return self.user.email
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .user_manager import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, verbose_name="email")
+    name = models.CharField(max_length=150, blank=True, verbose_name="Name")
+    role = models.CharField(max_length=10, default="user", verbose_name="Role")
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        User, related_name='user', on_delete=models.CASCADE
+    )
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(
+        verbose_name='Date of Birth', null=True, blank=True
+    )
+
+    @property
+    def email(self) -> str:
+        return self.user.email
+
+    def __str__(self):
+        return f'user_id={self.id} email={self.email}'
