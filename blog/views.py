e419034b83cf9acbd87d8e2ff1851e9a032c3931
inn-u <inna.yu10@gmail.com>
2025-06-09 23:13:40 +0300

    main page reainement

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,11 @@ from .models import Post
 class HomePageView(TemplateView):
     template_name = 'index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.order_by('-published_date')[:8]
+        return context
+
 
 class SingePostView(DetailView):
     template_name = 'post-single.html'
